d9c8e433346881e59796ff8b7d728dfe93e85c46
shin_li@foxmail.com <shin_li@foxmail.com>
2025-12-24 15:03:21 +0800

    First submit for test 2025-12-24

--- test.R
+++ test.R
@@ -31,8 +31,16 @@ help(plot_line_multipliers)
 
 # 【使用 git 命令行上传项目到 GitHub（以 R 包为例）】 https://www.bilibili.com/video/BV1Zu4y1i7uN/?share_source=copy_web&vd_source=3b82cc67f255864d8b26ae8a0de7e0b0
 
-# CMD窗口：
-# cd D:/learning-materials/02. 学术论文及方法相关/package/QARDLFlex
-#
+# Terminal窗口：
+# git init
+# git add .
+# git commit -m "First submit for test 2025-12-24"
+# git branch -M main
+# git remote add origin git@github.com:Passenger53/QARDLFlex.git
+# git push -u origin main
+
+devtools::install_github("Passenger53/QARDLFlex")
+
+
 
 
